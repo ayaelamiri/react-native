8cb423652c95bbd5806c010b47c44ea45eded277
ayaelamiri <70856931+ayaelamiri@users.noreply.github.com>
2020-09-08 02:23:44 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -2,14 +2,19 @@
 
 import React from 'react'
 import Navigation from './Navigation/Navigation'
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 import Store from './Store/configureStore'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/es/integration/react'
 
 export default class App extends React.Component {
   render() {
+    let persistor = persistStore(Store)
     return (
       <Provider store={Store}>
-        <Navigation/>
+        <PersistGate persistor={persistor}>
+          <Navigation />
+        </PersistGate>
       </Provider>
     )
   }
