01818fc6b2e1817c7d61d28a27afd1d43e3cb723
ayaelamiri <70856931+ayaelamiri@users.noreply.github.com>
2020-09-08 02:24:10 +0100

    Add files via upload

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -19,3 +19,10 @@ return fetch('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + API_TOKE
   .then((response) => response.json())
   .catch((error) => console.error(error));
 }
+
+// Récupération des meilleurs films
+export function getBestFilmsFromApi (page) {
+  return fetch('https://api.themoviedb.org/3/discover/movie?api_key=' + API_TOKEN + '&vote_count.gte=1000&sort_by=release_date.desc&language=fr&page=' + page)
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+}
